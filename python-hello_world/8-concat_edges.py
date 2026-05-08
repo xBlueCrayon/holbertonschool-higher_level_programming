40715aa661cd68dcde0a152ea5dee0b65e9ddcc7
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-08 11:50:32 +0400

    task 7 9

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str * 3)
-print(str[:9])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[68:74])
