3f27e8d9ceb1d6c771eddb24b7a7efe4e9f36538
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-08 10:45:30 +0400

    .

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,30 @@
+#print ("Hello world")
+
+#print(f"{98} Battery street")
+
+#print(f"{98} Battery street, {'San Francisco'}")
+
+a = "Python is cool"
+#print(a[4])
+
+a = "Python is cool"
+#print(a[0:6])
+
+a = "Python is cool"
+#print(a[:6])
+
+a = "Python is cool"
+#print(a[7:])
+
+a = "Python is cool"
+#print(a[7:-5])
+
+a = "Python is cool"
+#print(a[-2])
+
+
+name = "Diano"
+print("Hello", name)
+
+
+
