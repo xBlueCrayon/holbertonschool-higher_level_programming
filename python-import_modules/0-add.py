45d52aa1ed9358ca74076138d90efe39c862eaf9
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-08 15:46:11 +0400

    task 1 20

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """Return the addition of a and b"""
-    return (a + b)
+add = __import__('add_0').add
+
+a = 1
+b = 2
+
+print("{} + {} = {}".format(a, b, add(a, b)))
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-    """Return the addition of a and b"""
+    """My addition function"""
     return (a + b)
