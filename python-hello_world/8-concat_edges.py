432822d144313f60bac69298f7bc33d084a89956
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-08 11:49:31 +0400

    task 7 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Holberton School"
-print(str[10:] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str * 3)
+print(str[:9])
