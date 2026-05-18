fb4248eb775eb13046740fb42681a77ac6b3966c
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-18 11:51:48 +0400

    task 2 1

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Addition"""
+    return (a + b)
+
+
+def sub(a, b):
+    """Subtraction"""
+    return (a - b)
+
+
+def mul(a, b):
+    """Multiplication"""
+    return (a * b)
+
+
+def div(a, b):
+    """Division"""
+    return int(a / b)
+
