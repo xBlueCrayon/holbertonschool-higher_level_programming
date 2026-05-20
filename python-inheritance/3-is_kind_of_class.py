de013ab21c09fecef2e8e5904fd18a4c9c1ec369
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 17:24:35 +0400

    12 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines is_kind_of_class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or inherited."""
+
+    return isinstance(obj, a_class)
