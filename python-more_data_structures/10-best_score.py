57fde0814ed5eae0f1d452d112484bcc2a50bb20
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 14:41:58 +0400

    task 7 11

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    best_student = None
+    best_value = 0
+
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_value = value
+            best_student = key
+
+    return best_student
