536cf7b82804338584c90d694f86a5daaf1df7f2
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 15:54:08 +0400

    10 2 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
+
+
 class Square:
+    """This class defines a square."""
+
     def __init__(self, size):
+        """Initialize the square with a private size."""
         self.__size = size
