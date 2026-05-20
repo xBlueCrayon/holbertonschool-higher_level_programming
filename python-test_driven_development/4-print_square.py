e650aca06d8e929639e02d7885f85ba24a3f3388
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 15:13:11 +0400

    9 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for printing a square."""
+
+
+def print_square(size):
+    """Prints a square with the character #."""
+
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+
+    if isinstance(size, float) and not size.is_integer():
+        raise TypeError("size must be an integer")
+
+    size = int(size)
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
