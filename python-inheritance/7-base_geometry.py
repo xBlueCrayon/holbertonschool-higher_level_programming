4c1d2899fcca1d1c6294fcd829b24f1128dbc093
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 17:29:08 +0400

    12 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """This class defines a base geometry."""
+
+    def area(self):
+        """Raise an exception because area is not implemented."""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is an integer greater than 0."""
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
