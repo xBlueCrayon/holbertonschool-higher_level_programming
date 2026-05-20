e7a6a7064eb45d9048f81f46ceab6dec82a90af1
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 17:39:09 +0400

    12 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square."""
+
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description."""
+
+        return "[Square] {}/{}".format(self.__size,
+                                       self.__size)
