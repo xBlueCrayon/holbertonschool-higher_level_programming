76054b0ac71213a59eb9284ab3838b0321327afd
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-20 17:27:16 +0400

    12 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """This class defines a base geometry."""
+
+    def area(self):
+        """Raise an exception because area is not implemented."""
+
+        raise Exception("area() is not implemented")
