f9a132e19acbb724fa5e4d1ec67f71aade7e954b
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-21 08:44:52 +0400

    16 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Simple HTTP server module."""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    """Handle HTTP requests."""
+
+    def do_GET(self):
+        """Handle GET requests."""
+
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/status":
+            data = {
+                "status": "OK"
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/info":
+            data = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+
+            self.wfile.write(b"Endpoint not found")
+
+
+if __name__ == "__main__":
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+
+    print("Server running on port 8000...")
+    httpd.serve_forever()
