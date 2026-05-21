1479a52fe2f5230215d3e8a486a3db4072b27f3d
Akshay Gopal <akshay-gopal@hotmail.com>
2026-05-21 08:26:01 +0400

    14 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for JSON string conversion."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)."""
+
+    return json.dumps(my_obj)
